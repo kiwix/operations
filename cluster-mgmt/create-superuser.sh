c61bebd8bd7d1d0b710b3e962a219b6da51c794e
renaud gaudin <reg@rskg.org>
2022-03-24 22:39:35 +0000

    Split create-superuser into two scripts
    - a simple create-user.sh that works for any user and doesn't add any permission
    - create-superuser.sh nows calls it and adds a clusterrolebinding

--- cluster-mgmt/create-superuser.sh
+++ cluster-mgmt/create-superuser.sh
@@ -1,84 +1,30 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
 
 # Create a *superuser* for the cluster.
 # Permissions are similar to the default admin of the cluster that scaleway provides
 # credentials for.
 # Difference is that this one will be properly identified and easier to revoke.
-# Requires the scaleway-provided config to be at $adminconfpath path (see below).
+# Requires the scaleway-provided config to be at $ADMIN_CONF_PATH path (see below).
+
+set -e
 
 USERNAME=$1
-CLUSTERURL=https://api.scw.k8s.kiwix.org:6443
-NAMESPACE=default
-GROUPNAME=cluster-admin
-### CONFIG-END
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ADMIN_CONF_PATH=~/.kube/scw-admin_kiwix-prod.config
 
-
-if [ -z "$USERNAME" ] ; then
-  printf "Missing username.\nUsage: $0 USERNAME"
+if [ ! -f $ADMIN_CONF_PATH ] ; then
+  echo "Expects cluster admin config at ${ADMIN_CONF_PATH}"
   exit 1
 fi
+KUBECONFIG=${ADMIN_CONF_PATH}
 
-adminconfpath=~/.kube/scw-admin_kiwix-prod.config
-
-if [ ! -f $adminconfpath ] ; then
-  echo "Expects cluster admin config at ${adminconfpath}"
-  exit 1
-fi
-
-function kube {
-  kubectl --kubeconfig $adminconfpath "$@"
-}
-
-CA_CERT="users/ca.crt"
-
-if [ ! -f $CA_CERT ] ; then
-  echo "Retrieving CA certificate first"
-  kube get configmap kube-root-ca.crt -o json |jq -r '.data."ca.crt"' > $CA_CERT
-fi
-
-echo "Creating certificate for User $USERNAME"
-CERTIFICATE_NAME="user-${USERNAME}"
-CONFIGNAME="users/${USERNAME}_kiwix-prod.config"
-CSR_FILE="users/${USERNAME}.csr"
-KEY_FILE="users/${USERNAME}.key"
-CRT_FILE="users/${USERNAME}.crt"
-
-openssl genrsa -out ${KEY_FILE} 4096
-
-openssl req -new -key $KEY_FILE -out $CSR_FILE -subj "/CN=$USERNAME/O=$GROUPNAME"
-
-cat <<EOF | kube create -f -
-apiVersion: certificates.k8s.io/v1
-kind: CertificateSigningRequest
-metadata:
-  name: $CERTIFICATE_NAME
-spec:
-  groups:
-  - system:authenticated
-  request: $(cat $CSR_FILE | base64 | tr -d '\n')
-  signerName: kubernetes.io/kube-apiserver-client
-  # one year expiration
-  expirationSeconds: 31536000
-  usages:
-  - client auth
-EOF
-
-kube certificate approve $CERTIFICATE_NAME
-
-kube get csr $CERTIFICATE_NAME -o jsonpath='{.status.certificate}' | base64 -d > $CRT_FILE
-
-echo "Generate kubeconfig for $USERNAME"
-kubectl config --kubeconfig $CONFIGNAME set-credentials "${USERNAME}_kiwix-prod" --client-key=$KEY_FILE --client-certificate=$CRT_FILE --embed-certs=true
-kubectl config --kubeconfig $CONFIGNAME set-cluster kiwix-prod --embed-certs --certificate-authority=$CA_CERT --server=$CLUSTERURL
-kubectl config --kubeconfig $CONFIGNAME set-context "${USERNAME}@kiwix-prod" --cluster=kiwix-prod --user="${USERNAME}_kiwix-prod" --namespace=$NAMESPACE
-kubectl config --kubeconfig $CONFIGNAME use-context "${USERNAME}@kiwix-prod"
+echo "Create User ${USERNAME}…"
+KUBECONFIG=$KUBECONFIG $SCRIPT_DIR/create-user.sh $USERNAME
 
 echo "Add cluster-admin ClusterRole to User $USERNAME (using cluster-admins binding)"
 echo "> failure to add probably means it exists and must be updated manually (append user to subjects)"
 
-cat <<EOF | kube create -f -
+cat <<EOF | kubectl create -f -
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
---
+++ cluster-mgmt/create-user.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+USERNAME=$1
+NAMESPACE=$2
+GROUPNAME=$3
+CLUSTERURL=https://api.scw.k8s.kiwix.org:6443
+NAMESPACE="${NAMESPACE:=default}"
+GROUPNAME="${GROUPNAME:=users}"  # users | gh-bots
+### CONFIG-END
+
+
+if [ -z "$USERNAME" ] ; then
+  printf "Missing username.\nUsage: $0 USERNAME [NAMESPACE, [GROUPNAME]]"
+  exit 1
+fi
+
+echo "Ready for User creation with username=${USERNAME}, namespace=${NAMESPACE}, groupname=${GROUPNAME}. OK ?"
+read
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CA_CERT="${SCRIPT_DIR}/users/ca.crt"
+
+if [ ! -f $CA_CERT ] ; then
+  echo "Retrieving CA certificate first"
+  kubectl get configmap kube-root-ca.crt -o json |jq -r '.data."ca.crt"' > $CA_CERT
+fi
+
+echo "Creating certificate for User ${USERNAME} with Group ${GROUPNAME}"
+CERTIFICATE_NAME="user-${USERNAME}"
+CONFIGNAME="${SCRIPT_DIR}/users/${USERNAME}_kiwix-prod.config"
+CSR_FILE="${SCRIPT_DIR}/users/${USERNAME}.csr"
+KEY_FILE="${SCRIPT_DIR}/users/${USERNAME}.key"
+CRT_FILE="${SCRIPT_DIR}/users/${USERNAME}.crt"
+
+openssl genrsa -out ${KEY_FILE} 4096
+
+openssl req -new -key $KEY_FILE -out $CSR_FILE -subj "/CN=$USERNAME/O=$GROUPNAME"
+
+cat <<EOF | kubectl create -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: $CERTIFICATE_NAME
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat $CSR_FILE | base64 | tr -d '\n')
+  signerName: kubernetes.io/kube-apiserver-client
+  # one year expiration
+  expirationSeconds: 31536000
+  usages:
+  - client auth
+EOF
+
+kubectl certificate approve $CERTIFICATE_NAME
+
+kubectl get csr $CERTIFICATE_NAME -o jsonpath='{.status.certificate}' | base64 -d > $CRT_FILE
+
+echo "Generate kubeconfig for ${USERNAME}_kiwix-prod on namespace ${NAMESPACE}"
+kubectl config --kubeconfig $CONFIGNAME set-credentials "${USERNAME}_kiwix-prod" --client-key=$KEY_FILE --client-certificate=$CRT_FILE --embed-certs=true
+kubectl config --kubeconfig $CONFIGNAME set-cluster kiwix-prod --embed-certs --certificate-authority=$CA_CERT --server=$CLUSTERURL
+kubectl config --kubeconfig $CONFIGNAME set-context "${USERNAME}@kiwix-prod" --cluster=kiwix-prod --user="${USERNAME}_kiwix-prod" --namespace=$NAMESPACE
+kubectl config --kubeconfig $CONFIGNAME use-context "${USERNAME}@kiwix-prod"
