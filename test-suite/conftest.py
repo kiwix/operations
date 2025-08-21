e519aa762b892f4d4b3afe5d1b3e181e4678c3ad
rgaudin <reg@rskg.org>
2025-08-21 16:08:15 +0000

    Added-back CA mirror as APK expecting

--- test-suite/conftest.py
+++ test-suite/conftest.py
@@ -32,6 +32,7 @@ EXPECTED_APK_MIRRORS: list[str] = [
     "md.mirrors.hacktegic.com",
     "mirror-sites-fr.mblibrary.info",
     "mirror-sites-in.mblibrary.info",
+    "mirror-sites-ca.mblibrary.info",
     "mirror.triplebit.org",
 ]
 ZIM_MIRRORS: list[Mirror] = get_current_mirrors(
